a140c473753286c75702e2c13698df8bb35d2404
Isaac Melo <isaacalvesx7@gmail.com>
2025-12-10 18:01:50 -0300

    about_me text change

--- src/pages/home/aboutMe/description/index.tsx
+++ src/pages/home/aboutMe/description/index.tsx
@@ -5,28 +5,20 @@ import { fadeInUp, staggerContainer } from "./animations";
 export default function Description() {
   return (
     <AboutMeSection>
-      <motion.div
-        variants={staggerContainer}
-        initial="initial"
-        animate="animate"
-        transition={{ delay: 0.8 }}
-      >
+      <motion.div variants={staggerContainer} initial="initial" animate="animate" transition={{ delay: 0.8 }}>
         <motion.div variants={fadeInUp}>
           <Section title="Who am I?" />
         </motion.div>
         <motion.h1 variants={fadeInUp}>ABOUT ME</motion.h1>
         <motion.p variants={fadeInUp}>
-          I'm 25 years old and live in Amapá. Since 2020 I've been working as a
-          <b>full-stack developer</b> with a focus on <b>front-end</b>, creating
-          modern, performant and scalable web solutions.
+          I'm 25 years old and live in Amapá. Since 2022 I've been working as a<b>full-stack developer</b> with a focus on <b>front-end</b>, creating modern,
+          performant and scalable web solutions.
         </motion.p>
         <br />
         <motion.p variants={fadeInUp}>
-          My journey began when I decided to change careers and, in just 6
-          months of intense dedication, I got my first job in the field. Since
-          then, I've been constantly evolving, accumulating experience in
-          complex end-to-end projects and seeking <b>continuous evolution</b>{" "}
-          both in my technical and interpersonal skills.
+          My journey began when I decided to change careers and, in just 6 months of intense dedication, I got my first job in the field. Since then, I've been
+          constantly evolving, accumulating experience in complex end-to-end projects and seeking <b>continuous evolution</b> both in my technical and
+          interpersonal skills.
         </motion.p>
         <br />
       </motion.div>
--- src/pages/home/carrer/trajectory/index.tsx
+++ src/pages/home/carrer/trajectory/index.tsx
@@ -7,13 +7,13 @@ import { fadeInUp, staggerContainer } from "./animations";
 export default function Trajectory() {
   const CareerData = [
     {
-      date: "06/2023 - Current",
+      date: "01/2023 - Current",
       title: "Triagil - Mid-level Full-Stack Developer",
       description:
         "I was promoted to mid-level full-stack developer, taking on the responsibility of mentoring new developers and also collaborating on end-to-end projects.",
     },
     {
-      date: "12/2020 - 06/2023",
+      date: "01/2022 - 01/2023",
       title: "Triagil - Junior Frontend Developer",
       description:
         "I joined Triágil as the company's first frontend developer, I had the opportunity to build the Autorizai dashboard from scratch with React and Typescript and take 100% responsibility for the project.",
@@ -22,12 +22,7 @@ export default function Trajectory() {
 
   return (
     <CarrerSection id="career">
-      <motion.div
-        variants={staggerContainer}
-        initial="initial"
-        animate="animate"
-        transition={{ delay: 3.4 }}
-      >
+      <motion.div variants={staggerContainer} initial="initial" animate="animate" transition={{ delay: 3.4 }}>
         <motion.div variants={fadeInUp}>
           <Section title="My Career" />
         </motion.div>
